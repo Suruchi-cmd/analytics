5259735f8f6e35f97757ab25f0fb4c173915c35b
Suruchi Pokhrel <pokhrelsuruchi@gmail.com>
2025-03-18 02:52:50 -0400

    New Vercel domain add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000","https://analytics-frontend-nine.vercel.app"]
 # CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOW_CREDENTIALS = True
 
